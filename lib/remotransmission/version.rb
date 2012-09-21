188e7a82529d8fd651be3a39b2134e6951ee02b8
Sunny Ripert <sunny@sunfox.org>
2012-09-21 18:30:28 +0200

    Introducing v1.0.0!

--- lib/remotransmission/version.rb
+++ lib/remotransmission/version.rb
@@ -1,8 +1,8 @@
 module RemoTransmission
   module Version #:nodoc:
-    MAJOR  = 0
+    MAJOR  = 1
     MINOR  = 0
-    TINY   = 2
+    TINY   = 0
     STRING = [MAJOR, MINOR, TINY] * '.'
   end
 end
